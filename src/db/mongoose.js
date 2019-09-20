9dd05804bfc9dbb7e097a4074c9b4da872e51472
Yuan Zhi <zy8715@mun.ca>
2019-09-20 01:13:37 -0230

    Add delete, update and read router for task and user

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,13 +2,3 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-api', { useNewUrlParser: true, useCreateIndex: true })
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
